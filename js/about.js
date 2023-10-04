89046fe98c14b13cd18cd680e16efc7c13687086
Andelson Lopez <69158247+l9dson-wq@users.noreply.github.com>
2023-10-04 02:11:10 -0400

    Some changes
    Some important changes that I 've been made to my portafolio

--- js/about.js
+++ js/about.js
@@ -14,14 +14,3 @@ go_back_home_link.addEventListener('mouseleave', () => {
   go_back_home_link.innerHTML = original_text;
 
 });
-
-const extra_information = document.getElementById('extra_infomation');
-const big_text_section_event = document.getElementById('big_text_section_event');
-const icon_direction_link = document.getElementById('icon_direction_link');
-
-icon_direction_link.addEventListener('click', () => {
-
-  big_text_section_event.style.marginBottom = '10%';
-  extra_information.style.display = 'block';
-
-});
